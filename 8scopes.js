81caeb9d6c8a194d5d178509e8e835d618a81711
yusuforcun <yusuf.orcun.ozd@gmail.com>
2026-04-27 10:39:49 +0300

    functions

---
+++ 8scopes.js
@@ -0,0 +1,29 @@
+window.name = 'Tayfun'
+
+let name = 's'
+let surname ='sa'
+
+function letsLearnScope(){
+    console.log(name,surname)
+    if(true){
+        let name = 'Gok'
+        let surname = 'Kan'
+        console.log(name,surname)
+    }
+    console.log(name,surname)
+}
+letsLearnScope()
+console.log(name,surname)
+
+const person = {
+    name:'mal',
+    surname:'bir',
+    gender:'female',
+    isMarried:'no',
+    skills:['html',
+        'js',
+        'css']
+    
+}
+
+console.log(person.gender)
